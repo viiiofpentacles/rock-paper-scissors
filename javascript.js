674afabfb32f8a5cc7cdbdee1f8ab58a37f8cd77
bea <chamoischamois@protonmail.com>
2022-01-27 23:11:19 +0800

    Added scorekeeping to game() function

--- javascript.js
+++ javascript.js
@@ -18,15 +18,15 @@ function playRound(playerSelection,computerSelection){
     playerSelection = playerSelection.toLowerCase();
     computerSelection = computerPlay(actions);
 
-    if (playerSelection=="rock"&&computerSelection=="paper"){return "You lose! Paper beats Rock.";} 
-    else if (playerSelection=="rock"&& computerSelection=="scissors"){return "You win! Rock beats Scissors";}
+    if (playerSelection=="rock"&&computerSelection=="paper"){return computerScore++;} 
+    else if (playerSelection=="rock"&& computerSelection=="scissors"){return playerScore++;}
     else if (playerSelection=="rock"&&computerSelection=="rock"){return "It's a draw!";}
     else if (playerSelection=="paper"&&computerSelection=="paper"){return "It's a draw!";}
-    else if (playerSelection=="paper"&&computerSelection=="scissors"){return "Your lose! Scissors beats Paper.";}
-    else if (playerSelection=="paper"&&computerSelection=="rock"){return "You win! Paper beats Rock.";}
-    else if (playerSelection=="scissors"&&computerSelection=="paper"){return "You win! Scissors beats paper.";}
+    else if (playerSelection=="paper"&&computerSelection=="scissors"){return computerScore++;}
+    else if (playerSelection=="paper"&&computerSelection=="rock"){return playerScore++;}
+    else if (playerSelection=="scissors"&&computerSelection=="paper"){return playerScore++;}
     else if (playerSelection=="scissors"&&computerSelection=="scssors"){return "It's a draw!";}
-    else if (playerSelection=="scissors"&&computerSelection=="rock"){return "You lose! Rock beats Scissors.";}
+    else if (playerSelection=="scissors"&&computerSelection=="rock"){return computerScore++;}
     else {return "Error! Please try again.";}
 
 }
@@ -36,9 +36,17 @@ function game(){
 
     let i=0;
     for (let i=0; i < 5; i++) {
-           console.log(playRound(playerSelection, computerSelection));
+           playRound(playerSelection, computerSelection);
            if(i==4){
             console.log("Game Over!");
+            console.log("Player Score: " + playerScore);
+            console.log("Computer Score: " + computerScore);
+            
+            if (playerScore>computerScore){
+                console.log("You win!")
+            } else {
+                console.log("You lose!")
+            }
         }
     }
 }
