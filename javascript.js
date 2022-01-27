3370b2d0dda739f75d849ef291bd6d66a5cbf833
bea <chamoischamois@protonmail.com>
2022-01-27 22:43:55 +0800

    Added function game() that loops RPS 5 times

--- javascript.js
+++ javascript.js
@@ -5,26 +5,43 @@ function computerPlay(actions){
     return actions[Math.floor(Math.random()*actions.length)];
 }
 
+
+let playerSelection;
+let computerSelection;
+
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerSelection,computerSelection){
+
+    playerSelection = window.prompt("Type rock, paper, or scissors.");
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerPlay(actions);
 
     if (playerSelection=="rock"&&computerSelection=="paper"){return "You lose! Paper beats Rock.";} 
     else if (playerSelection=="rock"&& computerSelection=="scissors"){return "You win! Rock beats Scissors";}
     else if (playerSelection=="rock"&&computerSelection=="rock"){return "It's a draw!";}
     else if (playerSelection=="paper"&&computerSelection=="paper"){return "It's a draw!";}
     else if (playerSelection=="paper"&&computerSelection=="scissors"){return "Your lose! Scissors beats Paper.";}
-    else if (playerSelection=="paper"&&computerSelection=="rock"){return "You win! Paper beats Rock,";}
+    else if (playerSelection=="paper"&&computerSelection=="rock"){return "You win! Paper beats Rock.";}
     else if (playerSelection=="scissors"&&computerSelection=="paper"){return "You win! Scissors beats paper.";}
     else if (playerSelection=="scissors"&&computerSelection=="scssors"){return "It's a draw!";}
     else if (playerSelection=="scissors"&&computerSelection=="rock"){return "You lose! Rock beats Scissors.";}
     else {return "Error! Please try again.";}
+
 }
 
-let playerSelection = "ROck";
 
-playerSelection = playerSelection.toLowerCase();
+function game(){
 
+    let i=0;
+    for (let i=0; i < 5; i++) {
+           console.log(playRound(playerSelection, computerSelection));
+           if(i==4){
+            console.log("Game Over!");
+        }
+    }
+}
 
-const computerSelection = computerPlay(actions);
-
-console.log(computerSelection)
-console.log(playRound(playerSelection,computerSelection))
+console.log(game())
+   
